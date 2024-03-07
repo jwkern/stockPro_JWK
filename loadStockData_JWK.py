ea4258dae3d23491a15f8c30617d0fae22aa227a
Joshua W. Kern <57602749+jwkern@users.noreply.github.com>
2024-03-07 15:28:11 -0600

    Update loadStockData_JWK.py

--- loadStockData_JWK.py
+++ loadStockData_JWK.py
@@ -5,9 +5,25 @@
 #Date: 03/07/24
 #
 
-######################### Just do the thing ##############################
-import yfinance as yahooFinance
 
-ticker = "META"
+import yfinance as yahooFinance
+import csv
+
+
+###########################Import ticker list##################################
+tickerData = open('/home/jwkern/Downloads/nasdaq100.csv')
+tickerData=csv.reader(tickerData)
+rows=[]
+for row in tickerData:
+    rows.append(row)
+
+tickerList=[]
+
+for i in range(1,len(rows)):
+    tickerList.append(rows[i][1])
+
+
+##########################3## Get the data ####################################
+ticker = tickerList[0]
 stock = yahooFinance.Ticker(ticker)
 stock.history(period="6mo").to_csv(f"stockData_{ticker}.csv")
